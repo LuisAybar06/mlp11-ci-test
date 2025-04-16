c1296c93a795f82f100eae2719099d203deb411d
PC <aluisaybar@gmail.com>
2025-04-15 20:46:40 -0500

    test v2

--- tests/test_main_v1.py
+++ tests/test_main_v1.py
@@ -5,7 +5,7 @@ class TestMain(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(1,2), 3)
         self.assertEqual(add(-1,1), 0)
-        self.assertEqual(add(1,2), 4)
+        self.assertEqual(add(1,2), 3)
 
 if __name__ == '__main__':
     unittest.main()
