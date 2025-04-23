ef2ac7e05d17fc4c881cd59f43714fa9bb3770cc
PC <aluisaybar@gmail.com>
2025-04-22 19:53:53 -0500

    test sesion_3 pipelie v2-2

--- my_app/my_app_v2.py
+++ my_app/my_app_v2.py
@@ -1,7 +1,10 @@
 from typing import List, Any
 
+
 def process_data(data):
     return
+
+
 def concatenate_strings(strings: List[str]) -> str:
     return ''.join(strings)
 
@@ -9,12 +12,14 @@ def concatenate_strings(strings: List[str]) -> str:
 def display_name(message: str):
     print(message)
 
+
 def main():
     data=process_data("example data")
     print(data)
     strings = ["hello", "world"]
     concatenate = concatenate_strings(strings)
     display_name(concatenate)
+
 
 if __name__=="__main__":
     main()
